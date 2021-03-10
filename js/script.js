4193c9e245adf04ea291b15c22af638428db6549
Versalles <toma.galvez@duocuc.cl>
2021-03-10 12:17:56 -0300

    archivo js, extrae y muestra foto de usuarios de api y su nombre

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
 var contenido = document.querySelector('#contenido');
 
 function traer(){
-    fetch('texto.txt')
-    .then(data => data.text())
+    fetch('https://randomuser.me/api/')
+    .then(res => res.json()) //los datos se traeran en JSON
     .then(data => {
-        contenido.innerHTML = `${data}`
+        console.log(data.results['0'])
+        contenido.innerHTML = `
+        <img src="${data.results['0'].picture.large}" class="img-fluid rounded-circle" width="100px">
+        <p>Nombre: ${data.results['0'].name.first}` + ` ` + `${data.results['0'].name.last}</p>` 
+        
     })
 }
+
+/*console.log(data.results) -> nos trae el array 
+completo con la información
+*/
